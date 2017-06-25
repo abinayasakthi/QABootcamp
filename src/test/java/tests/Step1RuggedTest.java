e0fdd273646f37f24588b2cc18b4972c29a8675a
thivyalakshimi <thivyae@thoughtworks.com>
2017-06-25 11:23:16 +0530

    Revert "Before and after annotation, extraction of methods"
    This reverts commit fc5f7acdd34cfb9c599afc3707aa49a6e8f394bb.

--- src/test/java/tests/Step1RuggedTest.java
+++ src/test/java/tests/Step1RuggedTest.java
@@ -1,75 +1,74 @@
 package tests;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import pages.ClientsPage;
-import pages.LoginPage;
 
 import java.util.concurrent.TimeUnit;
 
 public class Step1RuggedTest {
-
-    public WebDriver driver;
-    public ClientsPage clientsPage = new ClientsPage(driver);
-    public LoginPage loginPage = new LoginPage(driver);
-
     @Test
     public void testEditAccount() {
-        loginPage.login();
-        clientsPage.searchClient();
-        clientsPage.editClientDetails();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.navigate().to("http://accountsdemo.herokuapp.com");
+        driver.findElement(By.id("user_email")).sendKeys("letslearnandshare@gmail.com");
+        driver.findElement(By.id("user_password")).sendKeys("!abcd1234");
+        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.linkText("CLIENTS")).click();
+        driver.findElement(By.id("search")).sendKeys("Thoughtworks");
+        driver.findElement(By.className("searchBtn")).click();
+        driver.findElement(By.linkText("Select Client")).click();
+        driver.findElement(By.linkText("Edit Client Details")).click();
+        driver.findElement(By.id("client_contact_person_name")).clear();
+        driver.findElement(By.id("client_contact_person_name")).sendKeys("ThoughtworksNew");
+        driver.findElement(By.id("client_address")).clear();
+        driver.findElement(By.id("client_address")).sendKeys("twAddressNew");
+        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.cssSelector("span.glyphicon-log-out")).click();
+        driver.quit();
     }
 
     @Test
     public void testAddQuotationForClient() {
-        loginPage.login();
-        clientsPage.searchClient();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.navigate().to("http://accountsdemo.herokuapp.com");
+        driver.findElement(By.id("user_email")).sendKeys("letslearnandshare@gmail.com");
+        driver.findElement(By.id("user_password")).sendKeys("!abcd1234");
+        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.linkText("CLIENTS")).click();
+        driver.findElement(By.id("search")).sendKeys("Thoughtworks");
+        driver.findElement(By.className("searchBtn")).click();
+        driver.findElement(By.linkText("Select Client")).click();
         driver.findElement(By.linkText("Quotations")).click();
         driver.findElement(By.partialLinkText("Add New Quotation")).click();
         driver.findElement(By.id("quotation_order_placed_by")).sendKeys("Quotation1");
         driver.findElement(By.id("quotation_event_name")).sendKeys("Event1");
         driver.findElement(By.id("quotation_event_date")).sendKeys("30-12-2016");
-        updateQuotationDetails();
+        driver.findElement(By.name("commit")).click();
+        driver.switchTo().alert().accept();
+        driver.findElement(By.cssSelector("span.glyphicon-log-out")).click();
+        driver.quit();
     }
 
     @Test
     public void testServiceTaxSearch(){
-        loginPage.login();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.navigate().to("http://accountsdemo.herokuapp.com");
+        driver.findElement(By.id("user_email")).sendKeys("letslearnandshare@gmail.com");
+        driver.findElement(By.id("user_password")).sendKeys("!abcd1234");
+        driver.findElement(By.name("commit")).click();
         driver.findElement(By.linkText("TAXES")).click();
         driver.findElement(By.linkText("SERVICE TAXES")).click();
         driver.findElement(By.id("start_date")).sendKeys("16-02-2017");
         driver.findElement(By.id("end_date")).sendKeys("17-02-2017");
         driver.findElement(By.cssSelector("#end_date + input")).click();
-    }
-
-    @Before
-    public void setUp(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-    }
-
-    @After
-    public void tearDown(){
-        logout();
-        driver.close();
         driver.quit();
     }
-
-    private void logout() {
-        driver.findElement(By.cssSelector("span.glyphicon-log-out")).click();
-    }
-
-
-
-    private void updateQuotationDetails() {
-        driver.findElement(By.name("commit")).click();
-        driver.switchTo().alert().accept();
-    }
-
-
 }
